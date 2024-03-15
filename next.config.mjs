b42334a2ec71da3b072afa0cbdb4c65f9fcc2885
evanhays98 <evan.hays@epita.fr>
2024-03-15 18:57:47 +0100

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+
+
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    }
+}
+
+
+
+
+
+export default nextConfig;
