f7b47296b28fc52572fd1428b343d00432f3d49c
evanhays98 <evan.hays@epita.fr>
2024-05-09 13:21:40 +0200

    add article md

---
+++ src/app/admin/addArticle/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import React, {useState} from "react";
+import classes from './page.module.css'
+import ReactMarkdown from "react-markdown";
+
+const Page = () => {
+
+    const [article, setArticle] = useState("");
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.content}>
+                <div className={classes.container_editable}>
+                    <textarea className={classes.textarea} onChange={(e) => setArticle(e.target.value)}/>
+                </div>
+                <div className={classes.separator}/>
+
+                <div className={classes.container_view}>
+                    <ReactMarkdown className={classes.markdown}>{article}</ReactMarkdown>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from './page.module.css'
+
+const Admin = () => {
+
+    return (
+        <div className={classes.container}>
+            <h1>Admin Page</h1>
+        </div>
+    );
+};
+
+export default Admin;
