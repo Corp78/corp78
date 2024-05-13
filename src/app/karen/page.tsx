14dc298081eb6a91f0df1f2d12fca09ce489906f
evanhays98 <evan.hays@epita.fr>
2024-05-13 13:37:54 +0200

    add kr code for karen

---
+++ src/app/karen/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import {useRouter} from "next/navigation";
+import React, {useEffect} from "react";
+import classes from "./page.module.css";
+import {Loading} from "@/app/libs/core";
+
+const QuentinPage = () => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        // Redirect to the desired URL
+        router.push('https://www.doctolib.fr/ophtalmologue/maurepas/karen-bitton-chappe');
+    }, [router]);
+
+    return (
+        <div className={classes.container}>
+            <Loading/>
+        </div>
+    );
+};
+
+export default QuentinPage;
