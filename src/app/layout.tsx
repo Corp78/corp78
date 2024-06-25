215c20a5a19c52a9c70c749bf694fcc09b90188b
evanhays98 <evan.hays@epita.fr>
2024-06-25 11:51:57 +0200

    add viewport not scalable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type {Metadata} from "next";
+import type {Metadata, Viewport} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {ReactNode} from "react";
@@ -7,7 +7,14 @@ const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "Cabinet Ophtalmologique Pariwest à Maurepas, 78 | Spécialistes en Rétine et Paupière",
-    description: "Bienvenue au Cabinet Ophtalmologique Pariwest à Maurepas, dans le 78. Nos spécialistes, le Dr Quentin Hays et le Dr Karen Bitton-Chappe, offrent une expertise reconnue en chirurgie réfractive, cataracte, chirurgie des paupières, glaucome et autres troubles oculaires. Avec une approche personnalisée et des technologies de pointe, nous assurons des soins de qualité pour votre vision. Prenez rendez-vous dès maintenant pour une consultation. Nous sommes situés à proximité de la gare La Verrière, à 15 minutes à pied",
+    description: "Bienvenue au Cabinet Ophtalmologique Pariwest à Maurepas, dans le 78. Nos spécialistes, le Dr Quentin Hays et le Dr Karen Bitton-Chappe, offrent une expertise reconnue en chirurgie réfractive, cataracte, chirurgie des paupières, glaucome et autres troubles oculaires. Avec une approche personnalisée et des technologies de pointe, nous assurons des soins de qualité pour votre vision. Prenez rendez-vous dès maintenant pour une consultation. Nous sommes situés à proximité de la gare La Verrière, à 15 minutes à pied"
+};
+
+export const viewport: Viewport = {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
 };
 
 export default function RootLayout({
