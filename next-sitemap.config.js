cfcfdda94cd068e21918956587dd4f9b3beced08
evanhays98 <evan.hays@epita.fr>
2024-07-25 10:30:31 +0200

    change priority

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,31 @@
 // next-sitemap.js
 
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://ophtalmologie-maurepas.fr/',
-    generateRobotsTxt: true, // (optional)
-    exclude: ['/admin', '/admin/dashboard', '/admin/dashboard/addArticle', '/quentin', '/karen'], // (optional)
+
+const getPriority = (url) => {
+    switch (url) {
+        case '/':
+            return 1.0;
+        case '/cabinet':
+            return 0.9;
+        default:
+            return 0.7;
+    }
 }
+
+const customTransform = (config, url) => {
+    // Set priority to 1 for root URL
+    console.log(url);
+    return {
+        loc: url,
+        changefreq: 'daily',
+        priority: getPriority(url),
+    };
+};
+
+module.exports = {
+    siteUrl: 'https://ophtalmologie-maurepas.fr',
+    generateRobotsTxt: true,
+    exclude: ['/admin', '/admin/dashboard', '/admin/dashboard/addArticle', '/quentin', '/karen'],
+    transform: customTransform,
+};
